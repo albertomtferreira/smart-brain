3c40de133d0104fbe762084a017773cd1aecd9b2
albertomtferreira <60177437+albertomtferreira@users.noreply.github.com>
2020-04-01 09:07:55 +0100

    update

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -17,7 +17,7 @@ class Signin extends React.Component {
         this.setState({ signInPassword: event.target.value })
     }
     onSubmitSignIn = () => {
-        fetch('http://localhost:3001/signin', {
+        fetch('https://nameless-stream-84850.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
